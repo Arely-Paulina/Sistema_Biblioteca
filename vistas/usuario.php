58159ce855a6e5728113db4e99aec59892af60ed
WirisLife <90435860+WirisLife145@users.noreply.github.com>
2022-03-19 10:32:27 -0600

    Actualización de interfaz
    Same feeling that you received notified

--- vistas/usuario.php
+++ vistas/usuario.php
@@ -22,7 +22,7 @@ include "header.php";
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                      <h1 class="box-title">Usuario <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i>Agregar </button></h1>
+                      <h1 class="box-title">Usuario <button class="btn btn-primary" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i>Agregar </button></h1>
                     </div>
                     <!-- /.box-header -->
                     <!-- centro -->
